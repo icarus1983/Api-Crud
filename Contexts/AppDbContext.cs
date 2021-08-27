8e3096995e91b624c2550aa0c40a198bacd15f98
icarus1983 <68357194+icarus1983@users.noreply.github.com>
2021-08-26 20:59:53 -0400

    Inicializando proyecto

---
+++ Contexts/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using API_RESTful_CRUD.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_RESTful_CRUD.Contexts
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+
+
+    }
+}
---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,90 @@
+﻿using API_RESTful_CRUD.Contexts;
+using API_RESTful_CRUD.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_RESTful_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        //public AppDbContext Context { get; }
+
+        public ProductoController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return context.Producto.ToList();
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(string id)
+        {
+            var producto = context.Producto.FirstOrDefault(p=>p.pro_codigo==id);
+            return producto;
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Producto producto)
+        {
+            try 
+            {
+                context.Producto.Add(producto);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest();
+            }
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Producto producto)
+        {
+            if (producto.pro_codigo.Equals(id))
+            {
+                context.Entry(producto).State = EntityState.Modified;
+                context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var producto = context.Producto.FirstOrDefault(p => p.pro_codigo == id);
+            if (producto != null)
+            {
+                context.Producto.Remove(producto);
+                context.SaveChanges();
+                return Ok();
+            }
+            else 
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
